7fdc4ea6c70ddca111ea9cc7d2734f0256c7f85d
Kevin LEIBA <kleiba@student.42.fr>
2017-06-12 13:03:24 +0200

    replaced logic with regex

--- src/index.js
+++ src/index.js
@@ -93,14 +93,13 @@ controller.hears(['aide', 'help'], 'direct_message,direct_mention,mention', func
  * @todo Display username after a command
  * @todo Make regex to be sure of what the user has wrote
  */
-controller.hears(['prépare une commande', 'préparer une commande', 'une commande'], 'direct_mention,mention', function (bot, message) {
-  const words = message.text.split(' ')
+controller.hears(['commande'], 'direct_mention,mention', function (bot, message) {
+  let regexpMinute = /\d{1,}\s{0,}min\w*/i
+  let regexpDuration = /\d*/i
+  let match = message.text.match(regexpMinute)
   let timer = 10
-  for (const word of words) {
-    if (Number(word)) {
-      timer = Number(word)
-      break
-    }
+  if (match) {
+    timer = Number(match[0].match(regexpDuration))
   }
   setTimeout(function () {
     console.log('\n\n\n======\n')
@@ -151,29 +150,6 @@ controller.hears(['bonjour', 'hey', 'salut', 'coucou'], 'direct_message,direct_m
   })
 })
 
-// Waiting for specific message
-// controller.hears(['hello', 'hi', 'salut', 'coucou'], 'direct_message,direct_mention,mention', function (bot, message) {
-//   bot.api.reactions.add({
-//     timestamp: message.ts,
-//     channel: message.channel,
-//     name: 'wave'
-//   }, function (err, res) {
-//     if (err) {
-//       bot.botkit.log('Failed to add emoji reaction :(', err)
-//     }
-//   })
-//
-//   controller.storage.users.get(message.user, function (err, user) {
-//     if (!err) {
-//       if (user && user.name) {
-//         bot.reply(message, 'Coucou ' + user.name + '!!')
-//       } else {
-//         bot.reply(message, 'Comment ça va la vie ?')
-//       }
-//     }
-//   })
-// })
-
 /**
  * Answers a basic question
  */
@@ -187,7 +163,7 @@ controller.hears(['ça va'], 'direct_message,direct_mention,mention', function (
  * Answers a basic question
  */
 controller.hears(['on mange quoi', 'manger'], 'direct_message,direct_mention,mention', function (bot, message) {
-  bot.reply(message, 'Tu peux me passer une commande si tu veux ! Pour savoir comment faire, demmande moi de l\'aide')
+  bot.reply(message, 'Tu peux me passer une commande si tu veux ! Pour savoir comment faire, demande moi de l\'aide')
 })
 
 /**
@@ -196,152 +172,3 @@ controller.hears(['on mange quoi', 'manger'], 'direct_message,direct_mention,men
 controller.hears(['rep'], 'direct_message,direct_mention,mention', function (bot, message) {
   responses.reactions.rep(bot, message)
 })
-
-/*
-controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_mention,mention', function (bot, message) {
-  var name = message.match[1]
-  controller.storage.users.get(message.user, function (err, user) {
-    if (!err) {
-      if (!user) {
-        user = {
-          id: message.user
-        }
-      }
-    }
-    user.name = name
-    controller.storage.users.save(user, function (err, id) {
-      if (!err) {
-        bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.')
-      }
-    })
-  })
-})
-
-controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention', function (bot, message) {
-  controller.storage.users.get(message.user, function (err, user) {
-    if (err) {
-      console.log(err)
-    }
-    if (user && user.name) {
-      bot.reply(message, 'Your name is ' + user.name)
-    } else {
-      bot.startConversation(message, function (err, convo) {
-        if (!err) {
-          convo.say('I do not know your name yet!')
-          convo.ask('What should I call you?', function (response, convo) {
-            convo.ask('You want me to call you `' + response.text + '`?', [
-              {
-                pattern: 'yes',
-                callback: function (response, convo) {
-                  // since no further messages are queued after this,
-                  // the conversation will end naturally with status == 'completed'
-                  convo.next()
-                }
-              },
-              {
-                pattern: 'no',
-                callback: function (response, convo) {
-                  // stop the conversation. this will cause it to end with status == 'stopped'
-                  convo.stop()
-                }
-              },
-              {
-                default: true,
-                callback: function (response, convo) {
-                  convo.repeat()
-                  convo.next()
-                }
-              }
-            ])
-
-            convo.next()
-          }, {'key': 'nickname'}) // store the results in a field called nickname
-
-          convo.on('end', function (convo) {
-            if (convo.status === 'completed') {
-              bot.reply(message, 'OK! I will update my dossier...')
-
-              controller.storage.users.get(message.user, function (err, user) {
-                if (err) {
-                  console.log(err)
-                }
-                if (!user) {
-                  user = {
-                    id: message.user
-                  }
-                }
-                user.name = convo.extractResponse('nickname')
-                controller.storage.users.save(user, function (err, id) {
-                  if (err) {
-                    console.log(err)
-                  }
-                  bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.')
-                })
-              })
-            } else {
-              // this happens if the conversation ended prematurely for some reason
-              bot.reply(message, 'OK, nevermind!')
-            }
-          })
-        }
-      })
-    }
-  })
-})
-
-controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function (bot, message) {
-  bot.startConversation(message, function (err, convo) {
-    if (err) {
-      console.log(err)
-    }
-    convo.ask('Are you sure you want me to shutdown?', [
-      {
-        pattern: bot.utterances.yes,
-        callback: function (response, convo) {
-          convo.say('Bye!')
-          convo.next()
-          setTimeout(function () {
-            process.exit()
-          }, 3000)
-        }
-      },
-      {
-        pattern: bot.utterances.no,
-        default: true,
-        callback: function (response, convo) {
-          convo.say('*Phew!*')
-          convo.next()
-        }
-      }
-    ])
-  })
-})
-
-controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
-'direct_message,direct_mention,mention', function (bot, message) {
-  var hostname = os.hostname()
-  var uptime = formatUptime(process.uptime())
-
-  bot.reply(message,
-    ':robot_face: I am a bot named <@' + bot.identity.name +
-    '>. I have been running for ' + uptime + ' on ' + hostname + '.')
-})
-
-function formatUptime (uptime) {
-  var unit = 'second'
-  if (uptime > 60) {
-    uptime = uptime / 60
-    unit = 'minute'
-  }
-  if (uptime > 60) {
-    uptime = uptime / 60
-    unit = 'hour'
-  }
-  if (uptime !== 1) {
-    unit = unit + 's'
-  }
-
-  uptime = uptime + ' ' + unit
-  return uptime
-}
-*/
